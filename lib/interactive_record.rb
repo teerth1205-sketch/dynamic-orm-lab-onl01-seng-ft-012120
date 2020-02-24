ad145a5f9b6faaed0a0253642803c77779199a1d
Teerth Patel <teerth1205@gmail.com>
2020-02-24 02:01:57 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,8 +49,8 @@ def values_for_insert
 end
 
 def save 
-   sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
+   sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)"
+    DB[:conn].execute(sql, values_for_insert)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end 
 end
