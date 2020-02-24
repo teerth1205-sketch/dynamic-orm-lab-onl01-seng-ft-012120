0448f971473aad4216fe687bd4815c36e9eadd50
Teerth Patel <teerth1205@gmail.com>
2020-02-24 02:18:06 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,7 +60,7 @@ def self.find_by_name(name)
 end 
 
 def self.find_by(att)
-  sql = "SELECT * FROM #{table_name} WHERE #{att.keys.t} = #{att.values}"
+  sql = "SELECT * FROM #{table_name} WHERE #{att.keys.join()} = #{att.values}"
   DB[:conn].execute(sql)
 end 
   
